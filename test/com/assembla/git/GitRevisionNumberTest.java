e3db95063dd631dcbf745df5f8c313867c1153ca
davie moston <daviemoston@gmail.com>
2008-06-05 23:27:55 +0100

    add a test that i forgot to commit

---
+++ test/com/assembla/git/GitRevisionNumberTest.java
@@ -0,0 +1,13 @@
+package com.assembla.git;
+
+import org.junit.Test;import static org.junit.Assert.assertEquals;
+
+public class GitRevisionNumberTest {
+    @Test
+    public void shouldBeAbleToStoreSha1Hash(){
+        String sha1 = "ed8bf4cece2834bd49dabc4c9423fbf4e4d2d54b";
+        GitRevisionNumber revision = new GitRevisionNumber(sha1);
+        assertEquals(sha1, revision.getNumber());
+    }
+    
+}
